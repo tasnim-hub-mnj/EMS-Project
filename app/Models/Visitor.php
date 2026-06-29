9b0f44da02a12fb7ac906b420cf5ff7c23d53737
hanan <hadialabood26@gmail.con>
2026-06-29 19:13:12 +0300

    add more modifications

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -6,11 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'profession',
+        'city',
+        'country',
+        'interests',
+        'preferred_language',
+    ];
+
+    protected $casts = [
+        'interests' => 'array',
+    ];
+    //==========================================
+    //الزائر له
 
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
 
     public function notifications()
     {
@@ -31,12 +48,23 @@ class Visitor extends Model
     {
         return $this->hasMany(VisitorSchedule::class);
     }
+    //===========================================
+    // الزائر له اجنحة محفوظة
 
     public function collectedBooths()
     {
         return $this->hasMany(CollectedBooths::class);
     }
-    //المفضلة كمان والتقييمات
+    //============================================
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    //التقييمات
+    //============================================
+
+    //الزائر له تذاكر دعم
     public function supportTickets()
     {
         return $this->hasMany(SupportTicket::class);
