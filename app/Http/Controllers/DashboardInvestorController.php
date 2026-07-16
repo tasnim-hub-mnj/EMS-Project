7fba07084a2c681982067b5b5fdc10c597edb5b2
Tasnim Almonjyd <tasnimit217@gmail.com>
2026-07-16 21:52:16 +0300

    add getExhibitionInvestorsStatistics function in class DashboardInvestorController extends Controller  for organizer

--- app/Http/Controllers/DashboardInvestorController.php
+++ app/Http/Controllers/DashboardInvestorController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booth;
 use App\Models\BoothBooking;
 use App\Models\Campaign;
 use App\Models\Event;
 use App\Models\Favorite;
+use App\Models\Investor;
 use App\Models\Report;
 use App\Models\SponsorshipBooking;
 use App\Models\Ticket;
@@ -37,7 +39,7 @@ class DashboardInvestorController extends Controller
                 ];
         }
     }
-
+    //=====================================================================
     //الأجنحة النشطة
     public function getActiveBoothsStats($investorId, $filter)
     {
@@ -81,8 +83,8 @@ class DashboardInvestorController extends Controller
             'growth' => round($growth, 1),
         ];
     }
-
-   // إجمالي حجوزات البوثات
+    //=====================================================================
+    // إجمالي حجوزات البوثات
     public function getTotalBookingsStats($investorId, $filter)
     {
         $range = $this->getDateRange($filter);
@@ -109,7 +111,7 @@ class DashboardInvestorController extends Controller
             'growth' => round($growth, 1),
         ];
     }
-
+    //=====================================================================
     //إجمالي التفاعل
     public function getTotalInteractionStats($investorId, $filter)
     {
@@ -179,7 +181,7 @@ class DashboardInvestorController extends Controller
             'growth' => round($growth, 1),
         ];
     }
-
+    //=====================================================================
     // الفعاليات المنشورة
     public function getTotalEventsStats($investorId, $filter)
     {
@@ -207,7 +209,7 @@ class DashboardInvestorController extends Controller
             'growth' => round($growth, 1),
         ];
     }
-    //__________________________________________________
+    //=====================================================================
     public function getDashboardStats(Request $request)//التابع الرئيسي
     {
         $filter = $request->filter ?? 'month';
@@ -220,5 +222,85 @@ class DashboardInvestorController extends Controller
             'total_events'      => $this->getTotalEventsStats($investorId, $filter),
         ], 200);
     }
+    //=====================================================================
+    //o
+    //=====================================================================
+    public function getExhibitionInvestorsStatistics()
+    {
+        $organizer = Auth::user()->organizer;
+        $exhibition_id = $organizer->exhibition->id;
+
+        //الشركات المشاركة في المعرض
+        $investors = Investor::whereHas('boothBookings', function ($q) use ($exhibition_id)
+        {
+            $q->whereHas('booth', function ($q2) use ($exhibition_id)
+            {
+                $q2->where('exhibition_id', $exhibition_id);
+            });
+        })->get();
+
+        $investor_ids = $investors->pluck('id');
+
+        //جميع حجوزات الأجنحة الخاصة بالمعرض
+        $booth_bookings = BoothBooking::whereIn('investor_id', $investor_ids)
+            ->whereHas('booth', function ($q) use ($exhibition_id)
+            {
+                $q->where('exhibition_id', $exhibition_id);
+            })
+            ->get();
+
+
+        $companies_count = $investors->count();
+        $booked_booths_count = $booth_bookings->count();//$organizer->exhibition->total_booths - $organizer->exhibition->available_booths
+        $total_value = $booth_bookings->sum('total_price');
+        $paid_amount = $booth_bookings->sum('paid_amount');
+
+        $companies_data = $investors->map(function ($inv) use ($booth_bookings)
+        {
+
+            $company_bookings = $booth_bookings->where('investor_id', $inv->id);
+
+            $company_total_value = $company_bookings->sum('total_price');
+            $company_paid_amount = $company_bookings->sum('paid_amount');
+            $rest = $company_total_value - $company_paid_amount;
+
+            $payment_rate = $company_total_value > 0
+                ? round(($company_paid_amount / $company_total_value) * 100, 2)
+                : 0;
+
+            $booths = $company_bookings->map(function ($booking)
+            {
+                return
+                [
+                    'booth_number' => $booking->booth->number,
+                    'area' => $booking->booth->area,
+                ];
+            });
+
+            return
+            [
+                'company_name' => $inv->company_name,
+                'email' => $inv->user->email,
+                'phone' => $inv->user->phone,
+
+                'total_value' => $company_total_value,
+                'paid_amount' => $company_paid_amount,
+                'payment_rate' => $payment_rate,
+                'rest' => $rest,
+
+                'booths' => $booths,
+            ];
+        });
+
+        return response()->json([
+            'companies_count' => $companies_count,
+            'booked_booths_count' => $booked_booths_count,
+            'total_value' => $total_value,
+            'paid_amount' => $paid_amount,
+            'companies' => $companies_data
+        ], 200);
+    }
+
+    //=====================================================================
 
 }
--- database/migrations/2026_06_02_185937_create_booth_bookings_table.php
+++ database/migrations/2026_06_02_185937_create_booth_bookings_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
     }
 
     /*
-     * Reverse the migrations
+     * Reverse the migrations.
      */
     public function down(): void
     {
